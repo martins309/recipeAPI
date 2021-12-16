9b70685ffd329cb29de0bd0c9c3c2901178ed906
Andrew Martins <andrew_omartins@yahoo.com>
2021-12-16 00:13:50 -0500

    since its an array of objects I used an array method. Its only returning one property and I need three

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,34 +1,26 @@
-const { application } = require('express');
-const express = require('express');
-const { reverse } = require('../data');
+const { application } = require("express");
+const express = require("express");
+const { reverse } = require("../data");
 const router = express.Router();
 
-
-
-
-const recipes = require('../data');
-
-router.get('/', (req, res) => {
-    res.send('The router seems to be working');
-
+const Recipes = require("../data");
+const recipeMap = Recipes.map((element) => {
+    return element.recipes
 })
 
-router.get('/recipes', (req, res) => {
-    const names = {
-        recipeNames: recipes
-   }
-   res.json(names);
-   
-})
+router.get("/", (req, res) => {
+  res.send("The router seems to be working");
+});
 
+router.get("/recipes", (req, res) => {
+//    const recipeNames = Recipes;
+    const recipeObj = {
+        recipeNames: recipeMap
+    }
+   res.json(recipeObj);
+});
 
-
-
-
-
-
-
-
-
-
+router.get('/recipes/details/:name'), (req, res) => {
+    
+}
 module.exports = router;
