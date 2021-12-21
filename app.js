743291e8a398dc8d73b417207f9d0db7a0fc10cf
Reuben Remy <reubenremy@gmail.com>
2021-12-21 13:14:47 -0500

    Swapped use of body-parser with built in body-parser in express.

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const express = require('express'),
-    bodyParser = require('body-parser'),
-    app = express();
+const express = require('express')
+
+app = express();
+
+// middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+// this line isn't needed because express version 4.16 & up have it built in so use the middle ware instead
+// bodyParser = require('body-parser'), <-- Delete this
+// app.use( bodyParser.urlencoded ({extended: false}));
+// app.use(bodyParser.json())
 
 const port = process.env.PORT || 3333;
-
-app.use( bodyParser.urlencoded ({extended: false}));
-app.use(bodyParser.json())
 
 app.use('/', require('./routes/recipes'));
 
 app.listen(port, () => {
     console.log(`The port is running on port ${port}`);
-
 })
